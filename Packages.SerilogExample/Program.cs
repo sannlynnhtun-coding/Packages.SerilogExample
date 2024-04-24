81a51ab40fb1422f7e763c038149ec8726ee4dbc
Sann Lynn Htun <sannlynnhtun.developer@gmail.com>
2024-04-24 09:23:19 +0630

    Add project files.

---
+++ Packages.SerilogExample/Program.cs
@@ -0,0 +1,27 @@
+﻿using Serilog;
+
+Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Debug()
+            .WriteTo.Console()
+            .WriteTo.File("logs/Packages.SerilogExample.txt", rollingInterval: RollingInterval.Hour)
+            .CreateLogger();
+
+Log.Information("Hello, world!");
+
+int a = 10, b = 0;
+try
+{
+    Log.Debug("Dividing {A} by {B}", a, b);
+    Log.Information("Dividing {A} by {B}", a, b);
+    Log.Verbose("Dividing {A} by {B}", a, b);
+    Log.Warning("Dividing {A} by {B}", a, b);
+    Console.WriteLine(a / b);
+}
+catch (Exception ex)
+{
+    Log.Error(ex, "Something went wrong");
+}
+finally
+{
+    await Log.CloseAndFlushAsync();
+}
